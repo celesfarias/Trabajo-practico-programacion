cd395760c9070f626b988b627297d936b5b92f68
ssandalias <santiagojara981@gmail.com>
2025-11-30 15:07:05 -0300

    TP Bubble y Selection

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -17,6 +17,59 @@ def init(vals):
     fase = "buscar"
 
 def step():
+    global items, n, i, j, min_idx, fase
+
+    # Terminó todo
+    if i >= n - 1:
+        return {"done": True}
+
+    # ------------------ FASE BUSCAR ------------------
+    if fase == "buscar":
+
+        # Comparación visual
+        a = min_idx
+        b = j
+
+        # Actualizar mínimo si corresponde
+        if items[j] < items[min_idx]:
+            min_idx = j
+
+        j += 1
+
+        # Si terminó el recorrido, pasamos a fase swap
+        if j >= n:
+            fase = "swap"
+
+        return {
+            "a": a,
+            "b": b,
+            "swap": False,
+            "done": False
+        }
+
+    # ------------------ FASE SWAP ------------------
+    if fase == "swap":
+
+        a = i
+        b = min_idx
+
+        hubo_swap = (min_idx != i)
+
+        if hubo_swap:
+            items[i], items[min_idx] = items[min_idx], items[i]
+
+        # Preparar siguiente pasada
+        i += 1
+        min_idx = i
+        j = i + 1
+        fase = "buscar"
+
+        return {
+            "a": a,
+            "b": b,
+            "swap": hubo_swap,
+            "done": False
+        }
     # TODO:
     # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
     #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
@@ -25,4 +78,4 @@ def step():
     #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
     #
     # Cuando i llegue al final, devolvé {"done": True}.
-    return {"done": True}
+    

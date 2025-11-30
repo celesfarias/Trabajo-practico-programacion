9413edefe0e36604d6ae2a09fafa0c33122a3af2
ssandalias <santiagojara981@gmail.com>
2025-11-30 16:14:29 -0300

    Selection Sort

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -19,33 +19,24 @@ def init(vals):
 def step():
     global items, n, i, j, min_idx, fase
 
-    # Terminó todo
     if i >= n - 1:
         return {"done": True}
 
     # ------------------ FASE BUSCAR ------------------
     if fase == "buscar":
 
-        # Comparación visual
         a = min_idx
         b = j
 
-        # Actualizar mínimo si corresponde
         if items[j] < items[min_idx]:
             min_idx = j
 
         j += 1
 
-        # Si terminó el recorrido, pasamos a fase swap
         if j >= n:
             fase = "swap"
 
-        return {
-            "a": a,
-            "b": b,
-            "swap": False,
-            "done": False
-        }
+        return {"a": a, "b": b, "swap": False, "done": False }
 
     # ------------------ FASE SWAP ------------------
     if fase == "swap":
@@ -58,18 +49,13 @@ def step():
         if hubo_swap:
             items[i], items[min_idx] = items[min_idx], items[i]
 
-        # Preparar siguiente pasada
         i += 1
         min_idx = i
         j = i + 1
         fase = "buscar"
 
-        return {
-            "a": a,
-            "b": b,
-            "swap": hubo_swap,
-            "done": False
-        }
+        return {"a": a, "b": b, "swap": hubo_swap, "done": False }
+    
     # TODO:
     # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
     #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
